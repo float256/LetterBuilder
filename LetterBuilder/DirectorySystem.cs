44e71ec8687d5df09f8c9d513994c65c446cf26b
Nikita Cheremin <a1za2z307@gmail.com>
2020-03-18 13:22:07 +0300

    Add main functionality for directory system

---
+++ LetterBuilder/DirectorySystem.cs
@@ -0,0 +1,271 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace LetterBuilder
+{
+    class DirectorySystem
+    {
+        public string ConnectionString { get; }
+        struct CatalogTableRow
+        {
+            public int id;
+            public string name;
+            public int idParentCatalog;
+
+            public CatalogTableRow(int id = 0, string name = null, int idParentCatalog = 0)
+            {
+                this.id = id;
+                this.name = name;
+                this.idParentCatalog = idParentCatalog;
+            }
+        };
+
+        struct TextBlockTableRow
+        {
+            public int id;
+            public string name;
+            public string text;
+            public int idParentCatalog;
+
+            public TextBlockTableRow(int id = 0, string name = null, string text = null, int idParentCatalog = 0)
+            {
+                this.id = id;
+                this.name = name;
+                this.text = text;
+                this.idParentCatalog = idParentCatalog;
+            }
+        };
+
+        public DirectorySystem(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        /// <summary>
+        ///     Добавление каталога в систему
+        /// </summary>
+        /// <param name="name">Имя каталога</param>
+        /// <param name="parentCatalogID">
+        ///     Индекс родительского каталога. В случае, если он не указан, ставится
+        ///     значение 0. Это означает, что каталог находится в корневой директории
+        /// </param>
+        public void AddCatalog(string name, int parentCatalogID = 0)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = $"INSERT INTO catalog VALUES ('{name}', {parentCatalogID})";
+                command.Connection = connection;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdateCatalog(int id, string name, int parentCatalogID)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = $"UPDATE catalog SET name='{name}', id_parent_catalog={parentCatalogID} WHERE id_catalog={id}";
+                command.Connection = connection;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteCatalog(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = $"DELETE FROM catalog WHERE id_catalog={id}";
+                command.Connection = connection;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        ///     Добавление текстового файла в систему в систему
+        /// </summary>
+        /// <param name="name">Имя файла</param>
+        /// <param name="text">Содержимое файла</param>
+        /// <param name="parentCatalogID">
+        ///     Индекс родительского каталога. В случае, если он не указан, ставится
+        ///     значение 0. Это означает, что файл находится в корневой директории
+        /// </param>
+        public void AddText(string name, string text, int parentCatalogID = 0)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = $"INSERT INTO text_block VALUES ('{name}', '{text}', {parentCatalogID})";
+                command.Connection = connection;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdateTextBlock(int id, string name, string text, int parentCatalogID)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = $"UPDATE text_block SET name='{name}', text='{text}', id_parent_catalog={parentCatalogID} WHERE id_text_block={id}";
+                command.Connection = connection;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteTextBlock(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = $"DELETE  FROM text_block WHERE id_text_block={id}";
+                command.Connection = connection;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        ///     Этот метод возвращает структуру системы каталогов
+        /// </summary>
+        /// <returns>
+        ///     Строка, содержащая структуру системы
+        /// </returns>
+        public string GetStructure()
+        {
+            Dictionary<int, CatalogTableRow> allCatalogInfo = new Dictionary<int, CatalogTableRow>();
+            Dictionary<int, List<int>> directoryChilds = new Dictionary<int, List<int>>();
+            Dictionary<int, TextBlockTableRow> allTextBlockInfo = new Dictionary<int, TextBlockTableRow>();
+            Dictionary<int, List<int>> directoryTextBlockAttachments = new Dictionary<int, List<int>>();
+
+            // Получение данных о каталогах и их запись в allCatalogInfo и directoryChilds.
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = $"SELECT * FROM catalog";
+                command.Connection = connection;
+                command.ExecuteNonQuery();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    CatalogTableRow currRow = new CatalogTableRow(
+                        (int)reader.GetValue(0), (string)reader.GetValue(1), (int)reader.GetValue(2));
+                    allCatalogInfo[currRow.id] = currRow;
+                    if (directoryChilds.ContainsKey(currRow.idParentCatalog))
+                    {
+                        directoryChilds[currRow.idParentCatalog].Add(currRow.id);
+                    }
+                    else
+                    {
+                        directoryChilds[currRow.idParentCatalog] = new List<int> { currRow.id };
+                    }
+                }
+            }
+
+            // Получение данных о текстовых файлах их запись в directoryTextBlockAttachments.
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = $"SELECT * FROM text_block";
+                command.Connection = connection;
+                command.ExecuteNonQuery();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    TextBlockTableRow currRow = new TextBlockTableRow(
+                        (int)reader.GetValue(0), (string)reader.GetValue(1), (string)reader.GetValue(2), (int)reader.GetValue(3));
+                    allTextBlockInfo[currRow.id] = currRow;
+                    if (directoryTextBlockAttachments.ContainsKey(currRow.idParentCatalog))
+                    {
+                        directoryTextBlockAttachments[currRow.idParentCatalog].Add(currRow.id);
+                    }
+                    else
+                    {
+                        directoryTextBlockAttachments[currRow.idParentCatalog] = new List<int> { currRow.id };
+                    }
+                }
+            }
+
+            // Составление результирующей строки.
+
+            if (allCatalogInfo.Count == 0)
+            {
+                return "";
+            }
+
+            // folderIndicesProcessingOrder - стек, содержащий порядок обработки папок.
+            // parentDirectoryIndices - стек, содержащий индексы всех родительских каталогов относительно текущей обрабатываемой папки.
+            Stack<int> folderIndicesProcessingOrder = new Stack<int>();
+            Stack<int> parentDirectoryIndices = new Stack<int>();
+            int depthLevel = 0;
+            string result = "";
+
+            // Вывод информации о файлах, находящихся в корневом каталоге
+            if (directoryTextBlockAttachments.ContainsKey(0))
+            {
+                foreach (int textBlockIdx in directoryTextBlockAttachments[0])
+                {
+                    result += $"{allTextBlockInfo[textBlockIdx].name} (text: {textBlockIdx})\n";
+                }
+            }
+
+            foreach (int item in directoryChilds[0])
+            {
+                folderIndicesProcessingOrder.Push(item);
+            }
+            while (folderIndicesProcessingOrder.Count != 0)
+            {
+                int currIdx = folderIndicesProcessingOrder.Peek();
+
+                // Если текущий обрабатываемый каталог находится в parentDirectoryIndices, то содержимое
+                // этой папки уже обработано и добавлено в result. Поэтому нужно удалить его из parentDirectoryIndices
+                // и folderIndicesProcessingOrder, и понизить уровень глубины папок.
+                if ((parentDirectoryIndices.Count != 0) && (parentDirectoryIndices.Peek() == currIdx))
+                {
+                    parentDirectoryIndices.Pop();
+                    folderIndicesProcessingOrder.Pop();
+                    depthLevel--;
+                }
+                else
+                {
+                    result += $"{new string(' ', depthLevel * 2)}{allCatalogInfo[currIdx].name} (catalog: {currIdx})\n";
+
+                    // Запись в результирующую строку информации о вложенных файлах текущей папки
+                    if (directoryTextBlockAttachments.ContainsKey(currIdx) && (directoryTextBlockAttachments[currIdx].Count != 0))
+                    {
+                        foreach (int textBlockIdx in directoryTextBlockAttachments[currIdx])
+                        {
+                            result += $"{new string(' ', depthLevel * 2 + 2)}{allTextBlockInfo[textBlockIdx].name} (text: {textBlockIdx})\n";
+                        }
+                    }
+
+                    // Если у папки есть вложенные папки, то они добавляются в parentDirectoryIndices,
+                    // т.е. их обработка начнется сразу после текущей итерации
+                    if (directoryChilds.ContainsKey(currIdx) && (directoryChilds[currIdx].Count != 0))
+                    {
+                        parentDirectoryIndices.Push(currIdx);
+                        foreach (int item in directoryChilds[currIdx])
+                        {
+                            folderIndicesProcessingOrder.Push(item);
+                        }
+                        depthLevel++;
+                    }
+                    else
+                    {
+                        folderIndicesProcessingOrder.Pop();
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LetterBuilder/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace LetterBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectorySystem dirSystem = new DirectorySystem(@"Data Source=DESKTOP-2U3C1KN\SQLEXPRESS;Initial Catalog=DirectoryStructure;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            if ((args.Length == 1) && (args[0] == "--help"))
+            {
+                Console.WriteLine("Available commands:\n" +
+                    "  --showtree - print all catalogs\n" +
+                    "  --add catalog\n" +
+                    "  --add text\n" +
+                    "  --delete catalog <id>\n" +
+                    "  --delete text <id>\n" +
+                    "  --update catalog <id>\n" +
+                    "  --update text <id>");
+            }
+            else if ((args.Length == 1) && (args[0] == "--showtree"))
+            {
+                Console.Write(dirSystem.GetStructure());
+            }
+            else if ((args.Length == 2) && (args[0] == "--add") && (args[1] == "catalog"))
+            {
+                Console.Write("Please enter catalog name: ");
+                string catalogName = Console.ReadLine();
+                Console.Write("Please enter parent catalog id: ");
+                int catalogId;
+                if (int.TryParse(Console.ReadLine(), out catalogId))
+                {
+                    dirSystem.AddCatalog(catalogName, catalogId);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Input");
+                }
+            }
+            else if ((args.Length == 2) && (args[0] == "--add") && (args[1] == "text"))
+            {
+                Console.Write("Please enter text block name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Please enter text: ");
+                string text = Console.ReadLine();
+
+                Console.Write("Please enter parent catalog id: ");
+                int catalogId;
+                if (int.TryParse(Console.ReadLine(), out catalogId))
+                {
+                    dirSystem.AddText(name, text, catalogId);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Input");
+                }
+            }
+            else if ((args.Length == 3) && (args[0] == "--update") && (args[1] == "catalog"))
+            {
+                int id, parentCatalogId;
+                Console.Write("Please enter catalog name: ");
+                string name = Console.ReadLine();
+                Console.Write("Please enter parent catalog id: ");
+                if (int.TryParse(Console.ReadLine(), out parentCatalogId) && int.TryParse(args[2], out id))
+                {
+                    dirSystem.UpdateCatalog(id, name, parentCatalogId);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Input");
+                }
+            }
+            else if ((args.Length == 3) && (args[0] == "--update") && (args[1] == "text"))
+            {
+                int id, catalogId;
+                Console.Write("Please enter block name: ");
+                string name = Console.ReadLine();
+                Console.Write("Please enter text: ");
+                string text = Console.ReadLine();
+                Console.Write("Please enter parent catalog id: ");
+                if (int.TryParse(Console.ReadLine(), out catalogId) && int.TryParse(args[2], out id))
+                {
+                    dirSystem.UpdateTextBlock(id, name, text, catalogId);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Input");
+                }
+            }
+            else if ((args.Length == 3) && (args[0] == "--delete"))
+            {
+                int id;
+                Console.Write("Are you sure? yes/no: ");
+                string answer = Console.ReadLine();
+                if (answer == "yes")
+                {
+                    if (int.TryParse(args[2], out id))
+                    {
+                        switch (args[1])
+                        {
+                            case "text":
+                                dirSystem.DeleteTextBlock(id);
+                                break;
+                            case "catalog":
+                                dirSystem.DeleteCatalog(id);
+                                break;
+                            default:
+                                Console.WriteLine("Incorrect Input");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect Input");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect command");
+            }
+        }
+    }
+}
