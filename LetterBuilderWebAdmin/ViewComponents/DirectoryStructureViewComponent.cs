5ec8d260f7d9be9bd92fa757db2d5235faa514b7
Nikita Cheremin <a1za2z307@gmail.com>
2020-04-12 02:33:48 +0300

    Added display of the file system structure

---
+++ LetterBuilderWebAdmin/ViewComponents/DirectoryStructureViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using LetterBuilderWebAdmin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace LetterBuilderWebAdmin.ViewComponents
+{
+    public class DirectoryStructureViewComponent : ViewComponent
+    {
+        private string _connectionString ;
+        private TextBlockRepository _textBlockRepository;
+        private CatalogRepository _catalogRepository;
+
+        public DirectoryStructureViewComponent(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("default");
+            _textBlockRepository = new TextBlockRepository(_connectionString);
+            _catalogRepository = new CatalogRepository(_connectionString);
+        }
+        public IViewComponentResult Invoke(int id)
+        {
+            CatalogContent result = new CatalogContent
+            {
+                Catalogs = _catalogRepository.GetSubcatalogsByParentCatalogId(id),
+                TextBlocks = _textBlockRepository.GetTextBlocksByParentCatalogId(id)
+            };
+            return View(result);
+        }
+    }
+}
