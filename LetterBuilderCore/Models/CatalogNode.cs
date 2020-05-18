49cb498194415acbfe16725d3904c3f02a8d86e3
Nikita Cheremin <a1za2z307@gmail.com>
2020-05-18 11:08:44 +0300

    Minor code formatting fixes

--- LetterBuilderCore/Models/CatalogNode.cs
+++ LetterBuilderCore/Models/CatalogNode.cs
@@ -6,17 +6,11 @@ namespace LetterBuilderCore.Models
     public class CatalogNode : ICatalogNode
     {
         public int Id { get; set; }
-        
         public string Name { get; set; }
-        
         public int Order { get; set; }
-        
         public List<ICatalogNode> ChildrenNodes { get; set; }
-        
         public List<TextBlock> CatalogAttachments { get; set; }
-
         public ICatalogNode ParentCatalog { get; set; }
-        
         public CatalogNode()
         {
             ChildrenNodes = new List<ICatalogNode>();
--- LetterBuilderWebApi/Dto/CatalogNodeDto.cs
+++ LetterBuilderWebApi/Dto/CatalogNodeDto.cs
@@ -8,13 +8,9 @@ namespace LetterBuilderWebApi.Dto
     public class CatalogNodeDto : ICatalogNode
     {
         public int Id { get; set; }
-        
         public string Name { get; set; }
-
         public int Order { get; set; }
-        
         public List<ICatalogNode> ChildrenNodes { get; set; }
-        
         public List<TextBlock> CatalogAttachments { get; set; }
 
         [JsonIgnore]
